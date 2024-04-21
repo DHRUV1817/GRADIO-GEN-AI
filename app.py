110c32377347fe3958c7016905a975d00ea0d12a
Nick088 <Nick088@users.noreply.huggingface.co>
2024-04-21 22:30:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import subprocess
 import os
 import gradio as gr
 from groq import Groq
 
-def generate_response(input_text):
-    client = Groq(
-    api_key=os.environ.get("Groq_Api_Key"),
-    )
+import gradio as gr
+from groq import Groq
 
+def generate_response(input_text, model, temperature, max_tokens, top_p):
+    client = Groq()
 
     stream = client.chat.completions.create(
         messages=[
             {"role": "system", "content": "you are a helpful assistant."},
             {"role": "user", "content": input_text}
         ],
-        model="mixtral-8x7b-32768",
-        temperature=0.5,
-        max_tokens=1024,
-        top_p=1,
+        model=model,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
         stop=None,
         stream=True,
     )
@@ -30,14 +29,18 @@ def generate_response(input_text):
 
     return response
 
-# Define the Gradio UI
-inputs = gr.Textbox(label="Enter your question")
-outputs = gr.Textbox(label="Model Response")
+# Define the Gradio chat interface
+additional_inputs = [
+    gr.Dropdown(choices=["mixtral-8x7b-32768", "mixtral-12x7b-32768"], label="Model"),
+    gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label="Temperature"),
+    gr.Slider(minimum=1, maximum=4096, step=1, label="Max Tokens"),
+    gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label="Top P"),
+]
 
-gr.Interface(
+gr.ChatInterface(
     fn=generate_response,
-    inputs=inputs,
-    outputs=outputs,
-    title="Language Model Assistant",
-    description="Ask questions and get responses from a language model.",
-).launch(show_api=False, share=True)
+    chatbot=gr.Chatbot(show_label=False, show_share_button=False, show_copy_button=True, likeable=True, layout="panel"),
+    additional_inputs=additional_inputs,
+    title="Groq API LLMs AI Models",
+    description="Using https://groq.com/ api, ofc as its free it will have some limitations so its better if you duplicate this space with your own api key<br>Hugging Face Space by [Nick088](https://linktr.ee/Nick088",
+).launch()
