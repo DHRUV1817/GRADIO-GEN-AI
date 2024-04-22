2bcefc7974e40fec689c7bb8d5ceb8fa41c256a8
Nick088 <Nick088@users.noreply.huggingface.co>
2024-04-22 00:07:11 +0000

    ugh (#1)
    - ugh (e9731f7d53fed363fa12924e7bfc45e14f850afe)
    
    
    Co-authored-by: Mikus <cappuch@users.noreply.huggingface.co>

--- app.py
+++ app.py
@@ -3,16 +3,33 @@ import random
 import gradio as gr
 from groq import Groq
 
+client = Groq(
+    api_key = os.environ.get("Groq_Api_Key")
+)
+    
+
 def generate_response(prompt, history, model, temperature, max_tokens, top_p, seed):
-    client = Groq(
-        api_key = os.environ.get("Groq_Api_Key")
-    )
+    messages = []
+    for i, data in enumerate(history):
+        if i % 2 == 0:
+            role = 'user'
+        else:
+            role = 'assistant'
+        message = {}
+        message["role"] = role
+        message["content"] = data
+        messages.append(message)
+
+    message = {}
+    message["role"] = "user" 
+    message["content"] = prompt
+    messages.append(message)
 
     if seed == 0:
         seed = random.randint(1, 100000)
     
     stream = client.chat.completions.create(
-        messages = history + [prompt],
+        messages=messages,
         model=model,
         temperature=temperature,
         max_tokens=max_tokens,
@@ -31,10 +48,9 @@ def generate_response(prompt, history, model, temperature, max_tokens, top_p, se
 
     return response
 
-# Define the Gradio chat interface
 additional_inputs = [
-    gr.Dropdown(choices=["llama3-70b-8192", "llama3-8b-8192", "mixtral-8x7b-32768", "llama2-70b-4096", "gemma-7b-it"], value="llama3-70b-8192", label="LLM Model"),
-    gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.5, label="Temperature", info="Controls randomness of responses"),
+    gr.Dropdown(choices=["llama3-70b-8192", "llama3-8b-8192", "mixtral-8x7b-32768", "llama2-70b-4096", "gemma-7b-it"], value="llama3-70b-8192", label="Model"),
+    gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.5, label="Temperature", info="Controls diversity of the generated text. Lower is more deterministic, higher is more creative."),
     gr.Slider(minimum=1, maximum=4096, step=1, value=4096, label="Max Tokens", info="The maximum number of tokens that the model can process in a single response"),
     gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.5, label="Top P", info="A method of text generation where a model will only consider the most probable next tokens that make up the probability p."),
     gr.Number(precision=0, value=42, label="Seed", info="A starting point to initiate generation, use 0 for random")
@@ -44,6 +60,6 @@ gr.ChatInterface(
     fn=generate_response,
     chatbot=gr.Chatbot(show_label=False, show_share_button=False, show_copy_button=True, likeable=True, layout="panel"),
     additional_inputs=additional_inputs,
-    title="Groq API LLMs AI Models",
-    description="Using https://groq.com/ api, ofc as its free it will have some limitations of requests per minute, so its better if you duplicate this space with your own api key<br>Hugging Face Space by [Nick088](https://linktr.ee/Nick088)",
+    title="Groq API UI",
+    description="Inference by Groq. Hugging Face Space by [Nick088](https://linktr.ee/Nick088)",
 ).launch()
