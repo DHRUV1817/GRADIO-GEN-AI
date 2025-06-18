ed98e2dd3a131bce57a8af1eb95a2a909bbee21b
Nymbo <Nymbo@users.noreply.huggingface.co>
2025-06-18 17:31:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ additional_inputs = [
             "meta-llama/llama-4-scout-17b-16e-instruct",
             "deepseek-r1-distill-llama-70b"
         ],
-        value="llama-3.1-70b-versatile",
+        value="llama-3.3-70b-versatile",
         label="Model"
     ),
     gr.Slider(
